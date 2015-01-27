4c55a5eaa4752c0ede2ce7e72594e9c7ea811dfa
Jan Klare <j.klare@x-ion.de>
2015-01-27 16:54:07 +0100

    set autoinstall_packages default to true

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['network_interfaces']['autoinstall_packages'] = false
+default['network_interfaces']['autoinstall_packages'] = true
 default['network_interfaces']['autoload_modules'] = false
